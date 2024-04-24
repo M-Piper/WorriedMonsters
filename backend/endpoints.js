e086859355ee492ea83834202e4678fa26b7cab2
M-Piper <margaret.f.piper@gmail.com>
2024-04-24 14:09:20 -0400

    localhost working again

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -308,7 +308,7 @@ export default function setupEndpoints(app) {
 
 
     app.get('/api/colours', (req, res) => {
-        const randomColourNumber = Math.floor(Math.random() * 47) + 1;
+        const randomColourNumber = Math.floor(Math.random() * 60) + 49;
         const colourQuery = 'SELECT main, darker, contrast FROM colours WHERE coloursID = ?';
 
         connection.query(colourQuery, [randomColourNumber], (err, colourResults) => {
