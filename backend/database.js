877190994468df784ac094012c87d0dc45b36847
Alex Wilmer <alex.wilmer@zensurance.com>
2024-04-25 19:16:12 -0600

    use railway mysql connection string

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,18 @@
-import mysql2 from 'mysql2';
-import dotenv from 'dotenv';
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
 dotenv.config();
 
-
-const urlDB = `mysql://${process.env.MYSQLUSER}: ${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+const urlDB = process.env.MYSQL_PRIVATE_URL;
 
 // Database connection setup
 const connection = mysql2.createConnection(urlDB);
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
 });
 
 // Export the connection object for use in other files
@@ -21,15 +20,15 @@ export { connection };
 
 // Function to start the database connection
 export function startDatabase() {
-    return new Promise((resolve, reject) => {
-        connection.connect(err => {
-            if (err) {
-                console.error('Error connecting to database:', err);
-                reject(err);
-                return;
-            }
-            console.log('Connected to MySQL database');
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    connection.connect((err) => {
+      if (err) {
+        console.error("Error connecting to database:", err);
+        reject(err);
+        return;
+      }
+      console.log("Connected to MySQL database");
+      resolve();
     });
+  });
 }
