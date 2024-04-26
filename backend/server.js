cda943885a66d9f6441b5620e9d6dfbedcc02708
Alex Wilmer <alex.wilmer@zensurance.com>
2024-04-25 19:10:03 -0600

    hello world endpint

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,48 @@
 import express from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
 import setupEndpoints from "./endpoints.js"; // Import endpoint setup function
 import { connection, startDatabase } from "./database.js";
+import cors from "cors";
 
 // Create an Express application
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// // Define CORS options
+// const corsOptions = {
+//     origin: 'https://worriedmonsters.com',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'DELETE'], // Add other methods you're using
+//     allowedHeaders: '*',
+// };
+//
+// // Enable CORS using the defined options
+// app.use(cors(corsOptions));
+
+// Handle preflight requests
+//app.options('*', cors(corsOptions));
 app.use(cors());
-
-
+console.log("test");
+// Use middleware to parse incoming request bodies
+// Increase the limit to 50MB (or any desired limit)
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Hello World!");
 });
 
 // Initialize database connection and start server
-startDatabase()/
-.then(() => {
-// Set up endpoints after database connection is established
-setupEndpoints(app);
+startDatabase()
+  .then(() => {
+    // Set up endpoints after database connection is established
+    setupEndpoints(app);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-})
-.catch(err => {
-    console.error('Error starting server:', err);
-});
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error starting server:", err);
+  });
